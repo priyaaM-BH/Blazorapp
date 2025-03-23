a286484f6dbf008f604734517bc3381c4e7c2333
Muthuvel, Priyaa <Priyaa.Muthuvel@bannerhealth.com>
2025-03-23 12:59:54 -0500

    so far

--- Sampleapp/Models/NavigationData.cs
+++ Sampleapp/Models/NavigationData.cs
@@ -3,6 +3,5 @@ namespace SampleApp.Models;
 public class NavigationData
 {
     public string seriesName { get; set; } = string.Empty;
-    public string pageDirective { get; set; } = string.Empty;
-    public List<string> pageSections { get; set; } = [];
+    public List<string> entryPointPageDirectives { get; set; } = [];
 }
--- Sampleapp/Services/BaseComponent.cs
+++ Sampleapp/Services/BaseComponent.cs
@@ -1,36 +1,34 @@
 using System.Text.Json;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using SampleApp.Models;
+using SampleApp.Models.ProviderModels;
 using SampleApp.Models.MainStateContainers;
+
 
 namespace SampleApp.Services
 {
 public class BaseComponent : ComponentBase
 {
     #region Permanent Series Data
-    public VisitReasonSeries _visitReasonSeries { get; set; } = new();
-    public InsuranceSeries _InsuranceSeries { get; set; } = new();
+    public Models.MainStateContainers.VisitReasonSeries _visitReasonSeries { get; set; } = new();
+    public Models.MainStateContainers.InsuranceSeries _InsuranceSeries { get; set; } = new();
     #endregion
 
     #region Temporary Series Data
-    public VisitReasonSeries _wipVisitReasonSeries { get; set; } = new();
+    public Models.MainStateContainers.VisitReasonSeries _wipVisitReasonSeries { get; set; } = new();
+    private Models.MainStateContainers.InsuranceSeries _wipInsuranceSeries { get; set; } = new();
+    public bool _inEdit = false;
 
-    private InsuranceSeries _wipInsuranceSeries { get; set; } = new();
     #endregion
 
     #region dependency injections
     [Inject]
     protected NavigationManager _navigationManager { get; set; } = default!;
-
-    [Inject]
-    protected IWebAssemblyHostEnvironment _WebHostEnvironment { get; set; }  = default!;
     #endregion
 
     #region initial Data from all http calls
-    public static ProviderData _ProviderInformation{ get; set; } = new();
+    public static Models.ProviderData _ProviderInformation{ get; set; } = new();
     //TODO: add portal user me, relationships, portal Application, insurance get coverages as public fields here
-    public static PatientInformationSeries _patientInformationSeries { get; set; } = new();
+    public static Models.MainStateContainers.PatientInformationSeries _patientInformationSeries { get; set; } = new();
     #endregion
 
 }
