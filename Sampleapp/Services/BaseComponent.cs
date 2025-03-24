9f5bb9de8076c0192628986a067b9611137ce463
Muthuvel, Priyaa <Priyaa.Muthuvel@bannerhealth.com>
2025-03-24 09:55:54 -0500

    Update BaseComponent.cs

--- Sampleapp/Services/BaseComponent.cs
+++ Sampleapp/Services/BaseComponent.cs
@@ -9,15 +9,15 @@ namespace SampleApp.Services
 public class BaseComponent : ComponentBase
 {
     #region Permanent Series Data
-    public Models.MainStateContainers.VisitReasonSeries _visitReasonSeries { get; set; } = new VisitReasonSeries();
-    public Models.MainStateContainers.InsuranceSeries _InsuranceSeries { get; set; } = new InsuranceSeries();
+    public static Models.MainStateContainers.VisitReasonSeries _visitReasonSeries { get; set; } = new VisitReasonSeries();
+    public static Models.MainStateContainers.InsuranceSeries _InsuranceSeries { get; set; } = new InsuranceSeries();
     public static Models.MainStateContainers.PatientInformationSeries _patientInformationSeries { get; set; } = new PatientInformationSeries();
     public static Models.ProviderData _ProviderInformation{ get; set; } = new Models.ProviderData();
     #endregion
 
     #region Temporary Series Data
-    public Models.MainStateContainers.VisitReasonSeries _wipVisitReasonSeries { get; set; } = new VisitReasonSeries();
-    public Models.MainStateContainers.InsuranceSeries _wipInsuranceSeries { get; set; } = new InsuranceSeries();
+    public static Models.MainStateContainers.VisitReasonSeries _wipVisitReasonSeries { get; set; } = new VisitReasonSeries();
+    public static Models.MainStateContainers.InsuranceSeries _wipInsuranceSeries { get; set; } = new InsuranceSeries();
     public static Models.MainStateContainers.PatientInformationSeries _wipPatientInformationSeries { get; set; } = new PatientInformationSeries();
     public bool _inEdit = false;
     public bool _isTeleHealth { get; set; }
