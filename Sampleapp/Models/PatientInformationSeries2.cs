984af816a613656dadeaedd3634d0d27b470504b
Muthuvel, Priyaa <Priyaa.Muthuvel@bannerhealth.com>
2025-03-24 20:06:19 -0500

    recent

--- Sampleapp/Models/PatientInformationSeries2.cs
+++ Sampleapp/Models/PatientInformationSeries2.cs
@@ -2,7 +2,6 @@
 
 public class PatientInformationSeries2
 {
-    public string? portalUserId { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
     public string Address { get; set; } = string.Empty;
 }
