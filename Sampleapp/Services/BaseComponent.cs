260c352888bc29442b34ccea2755959f5c6075da
Muthuvel, Priyaa <Priyaa.Muthuvel@bannerhealth.com>
2025-04-10 17:30:33 -0500

    Controller

--- Sampleapp/Services/BaseComponent.cs
+++ Sampleapp/Services/BaseComponent.cs
@@ -37,17 +37,78 @@ public class BaseComponent : ComponentBase
     //If then Sophia navigates to the next page, that page's oninitialize will add its page directive to the list.
     //
     //Any time Sophia hits the back button to go to a previous page, that page to remove the last entry (itself) in the list.
-    public static List<string> PatientSelectionPageDirectives { get; set; } = [];
-    public static List<string> AppointmentPageDirectives { get; set; } = [];
-    public static List<string> PatientInformationPageDirectives { get; set; } = [];
-    public static List<string> InsurancePageDirectives { get; set; } = [];
+   
 
     #endregion
 
-    #region initial Data from all http calls
-    
-    //TODO: add portal user me, relationships, portal Application, insurance get coverages as public fields here
-    
+    #region Page Directives
+    /// <summary>
+    /// This is set by the controller's On Initialize method which also initializes the ModulesAndPageDirectives list
+    /// </summary>
+    public static List<ModuleEntryPoints> ModuleEntryPointsList = [];
+    //These are the pages each module visited.  If appointment module gets invoked, the first page of the module
+    //will create a new list with one entry being the page directive of the visit reason page.
+    //
+    //If then Sophia navigates to the next page, that page's oninitialize will add its page directive to the list.
+    //
+    //Any time Sophia hits the back button to go to a previous page, that page to remove the last entry (itself) in the list.
+    public static List<ModuleEntryPoints> ModulePagesVisitedList { get; set; } = new List<ModuleEntryPoints>();
+    public class ModulePagesVisited
+    {
+        public List<string> PagesVisited { get; set; } = new();
+    }
+
+    public int GetModuleOffset(string moduleName)
+    {
+        return ModuleEntryPointsList.FindIndex(n => n.moduleName.ToLower() == moduleName.ToLower());
+    }
+
+    public static void AddPageDirective(string moduleName, string pageDirective)
+    {
+        // Find the pagedirective that matches the moduleName
+        bool containsSpecificEntry = ModulePagesVisitedList.Any(nav => nav.moduleName.Equals(moduleName, StringComparison.OrdinalIgnoreCase) &&
+                                            nav.entryPointPageDirectives.Contains(pageDirective));
+        // Find the index that matches the moduleName
+        int moduleIndex = ModulePagesVisitedList.FindIndex(n => n.moduleName.Equals(moduleName, StringComparison.OrdinalIgnoreCase));
+        if (moduleIndex != -1 && !containsSpecificEntry)
+        {
+            ModulePagesVisitedList[moduleIndex].entryPointPageDirectives.Add(pageDirective);
+        }
+        else if(moduleIndex == -1)
+        {
+            ModuleEntryPoints ModuleEntryPoints = new()
+            {
+                entryPointPageDirectives = [pageDirective],
+                moduleName = moduleName,
+            };
+            ModulePagesVisitedList.Add(ModuleEntryPoints);
+        }
+    }
+
+    public static void RemovePageDirective(string moduleName, string pageDirective)
+    {
+        // Find the ModuleEntryPoints that matches the moduleName
+        var moduleEntry = ModulePagesVisitedList
+            .FirstOrDefault(nav => nav.moduleName.Equals(moduleName, StringComparison.OrdinalIgnoreCase));
+
+        if (moduleEntry != null)
+        {
+            // Check if the pageDirective exists in the entryPointPageDirectives list
+            if (moduleEntry.entryPointPageDirectives.Contains(pageDirective))
+            {
+                // Remove the pageDirective
+                moduleEntry.entryPointPageDirectives.Remove(pageDirective);
+            }
+            else
+            {
+                Console.WriteLine($"'{pageDirective}' not found in module '{moduleName}'.");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Module '{moduleName}' not found.");
+        }
+    }
     #endregion
 
 }
